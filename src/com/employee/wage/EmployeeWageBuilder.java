f6440abdbd7125d5d569d404882d73c67f824808
Darpan <darpan.madlani@gmail.com>
2022-07-14 20:44:03 +0530

    OOPs concept

--- src/com/employee/wage/EmployeeWageBuilder.java
+++ src/com/employee/wage/EmployeeWageBuilder.java
@@ -1,54 +1,59 @@
 package com.employee.wage;
 
-
 public class EmployeeWageBuilder {
 
-	public static void main(String[] args) {
-		final int PRESENT = 1;
-		final int PART_TIME = 2;
-		final int FULL_TIME = 8;
-		final int WAGE_PER_HOUR = 20;
-		final int MAX_WORKING_DAYS = 20;
-		final int MAX_WORKING_HOUR = 60;
+	final int PRESENT = 1;
+	final int PART_TIME = 2;
+	final int FULL_TIME = 8;
+	final int WAGE_PER_HOUR = 20;
+	final int MAX_WORKING_DAYS = 20;
+	final int MAX_WORKING_HOUR = 60;
 
+	public int checkEmpAttendance(int totalWorkingHour) {
+		if (totalWorkingHour == MAX_WORKING_HOUR - (FULL_TIME / 2)) {
+			return PART_TIME;
+		} else {
+			return (int) (Math.random() * 3);
+		}
+	}
+
+	public int getWorkingHour(int isEmpPresent) {
+		switch (isEmpPresent) {
+		case PRESENT:
+			// System.out.println("Employee is present");
+			return FULL_TIME;
+
+		case PART_TIME:
+			// System.out.println("Employee is working part-time");
+			return FULL_TIME / 2;
+
+		default:
+			// System.out.println("Employee is absent");
+			return 0;
+		}
+	}
+
+	public double calculateEmployeeWage() {
 		int totalWorkingHour = 0;
 		int day = 0;
 		while (day < MAX_WORKING_DAYS && totalWorkingHour < MAX_WORKING_HOUR) {
 			day++;
-			int isEmpPresent = 0;
-			if (totalWorkingHour == MAX_WORKING_HOUR - (FULL_TIME / 2)) {
-				isEmpPresent = PART_TIME;
-			} else {
-				isEmpPresent = (int) (Math.random() * 3);
-			}
-			int empHr;
+			int isEmpPresent = checkEmpAttendance(totalWorkingHour);
 
-			// System.out.println(isEmpPresent);
-
-			switch (isEmpPresent) {
-
-			case PRESENT:
-				// System.out.println("Employee is present");
-				empHr = FULL_TIME;
-				break;
-
-			case PART_TIME:
-				// System.out.println("Employee is working part-time");
-				empHr = FULL_TIME / 2;
-				break;
-
-			default:
-				// System.out.println("Employee is absent");
-				empHr = 0;
-				break;
-
-			}
+			int empHr = getWorkingHour(isEmpPresent);
 
 			totalWorkingHour = totalWorkingHour + empHr;
 		}
-		int totalSalary = totalWorkingHour * WAGE_PER_HOUR;
-		System.out.println("Employee monthly salary : $" + totalSalary + " USD");
 		System.out.println("Working Hour : " + totalWorkingHour + " Hour");
+		return totalWorkingHour * WAGE_PER_HOUR;
+	}
+
+	public static void main(String[] args) {
+
+		EmployeeWageBuilder builder = new EmployeeWageBuilder();
+
+		double totalSalary = builder.calculateEmployeeWage();
+		System.out.println("Employee monthly salary : $" + totalSalary + " USD");
 	}
 
 }
