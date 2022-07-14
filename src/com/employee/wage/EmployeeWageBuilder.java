d60e4bcd090c9103a9d3f5217b58d7d54ecd4fc3
Darpan <darpan.madlani@gmail.com>
2022-07-14 20:59:54 +0530

    Multiple companies emp wage calculation

--- src/com/employee/wage/EmployeeWageBuilder.java
+++ src/com/employee/wage/EmployeeWageBuilder.java
@@ -4,28 +4,28 @@ public class EmployeeWageBuilder {
 
 	final int PRESENT = 1;
 	final int PART_TIME = 2;
-	final int FULL_TIME = 8;
-	final int WAGE_PER_HOUR = 20;
-	final int MAX_WORKING_DAYS = 20;
-	final int MAX_WORKING_HOUR = 60;
+	/*
+	 * final int FULL_TIME = 8; final int WAGE_PER_HOUR = 20; final int
+	 * MAX_WORKING_DAYS = 20; final int MAX_WORKING_HOUR = 60;
+	 */
 
-	public int checkEmpAttendance(int totalWorkingHour) {
-		if (totalWorkingHour == MAX_WORKING_HOUR - (FULL_TIME / 2)) {
+	public int checkEmpAttendance(int totalWorkingHour, int fullTimeWorkingHour, int maxWorkingHour) {
+		if (totalWorkingHour == maxWorkingHour - (fullTimeWorkingHour / 2)) {
 			return PART_TIME;
 		} else {
 			return (int) (Math.random() * 3);
 		}
 	}
 
-	public int getWorkingHour(int isEmpPresent) {
+	public int getWorkingHour(int isEmpPresent, int fullTimeWorkingHour) {
 		switch (isEmpPresent) {
 		case PRESENT:
 			// System.out.println("Employee is present");
-			return FULL_TIME;
+			return fullTimeWorkingHour;
 
 		case PART_TIME:
 			// System.out.println("Employee is working part-time");
-			return FULL_TIME / 2;
+			return fullTimeWorkingHour / 2;
 
 		default:
 			// System.out.println("Employee is absent");
@@ -33,27 +33,31 @@ public class EmployeeWageBuilder {
 		}
 	}
 
-	public double calculateEmployeeWage() {
+	public double calculateEmployeeWage(int fullTimeWorkingHour, int wagePerHour,
+			int maxWorkingDay, int maxWorkingHour) {
 		int totalWorkingHour = 0;
 		int day = 0;
-		while (day < MAX_WORKING_DAYS && totalWorkingHour < MAX_WORKING_HOUR) {
+		while (day < maxWorkingDay && totalWorkingHour < maxWorkingHour) {
 			day++;
-			int isEmpPresent = checkEmpAttendance(totalWorkingHour);
+			int isEmpPresent = checkEmpAttendance(totalWorkingHour, fullTimeWorkingHour, maxWorkingHour);
 
-			int empHr = getWorkingHour(isEmpPresent);
+			int empHr = getWorkingHour(isEmpPresent, fullTimeWorkingHour);
 
 			totalWorkingHour = totalWorkingHour + empHr;
 		}
 		System.out.println("Working Hour : " + totalWorkingHour + " Hour");
-		return totalWorkingHour * WAGE_PER_HOUR;
+		return totalWorkingHour * wagePerHour;
 	}
 
 	public static void main(String[] args) {
 
 		EmployeeWageBuilder builder = new EmployeeWageBuilder();
 
-		double totalSalary = builder.calculateEmployeeWage();
-		System.out.println("Employee monthly salary : $" + totalSalary + " USD");
+		double dMartEmpSalary = builder.calculateEmployeeWage(8, 20, 20, 60);
+		System.out.println("Employee monthly salary : $" + dMartEmpSalary + " USD");
+
+		double rilEmpSalary = builder.calculateEmployeeWage(9, 25, 22, 80);
+		System.out.println("Employee monthly salary : $" + rilEmpSalary + " USD");
 	}
 
 }
