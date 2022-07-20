98260fb8151900b6513922c2647ee8c97cd08c63
Darpan <darpan.madlani@gmail.com>
2022-07-20 20:39:06 +0530

    Store data with array

--- src/com/employee/wage/EmployeeTest.java
+++ src/com/employee/wage/EmployeeTest.java
@@ -6,12 +6,15 @@ public class EmployeeTest {
 	int lastIndex;
 
 	public EmployeeTest() {
-		companies = new CompanyEmpWage[10];
+		companies = new CompanyEmpWage[100];
 	}
 
-	public void addCompany(CompanyEmpWage company) {
-		company.calculateEmployeeWage();
-		companies[lastIndex] = company;
+	public void addCompany(String companyName, int fullTimeWorkingHour, int wagePerHour,
+			int maxWorkingDay, int maxWorkingHour) {
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName,
+				fullTimeWorkingHour, wagePerHour, maxWorkingDay, maxWorkingHour);
+		companyEmpWage.calculateEmployeeWage();
+		companies[lastIndex] = companyEmpWage;
 		lastIndex++;
 	}
 
@@ -27,15 +30,17 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 		EmployeeTest testObj = new EmployeeTest();
 
-		CompanyEmpWage dmartEmpWage = new CompanyEmpWage("DMart", 8, 20, 20, 60);
-
-		CompanyEmpWage rilEmpWage = new CompanyEmpWage("Reliance", 10, 25, 22, 80);
-
-		CompanyEmpWage wiproEmpWage = new CompanyEmpWage("Wipro", 8, 25, 20, 80);
-
-		testObj.addCompany(dmartEmpWage);
-		testObj.addCompany(rilEmpWage);
-		testObj.addCompany(wiproEmpWage);
+		testObj.addCompany("DMart", 8, 20, 20, 60);
+		testObj.addCompany("Reliance", 10, 25, 22, 80);
+		testObj.addCompany("Wipro", 8, 25, 20, 80);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
+		testObj.addCompany("Tesla", 12, 40, 25, 100);
 
 		testObj.printAllCompanies();
 	}
